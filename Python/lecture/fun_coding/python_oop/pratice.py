9b7fddb9a7c47fbaac129020485a46db51a7933b
jae hun hur <koni114@naver.com>
2021-04-23 23:47:39 -0700

    docs: modify practice.py

--- Python/lecture/fun_coding/python_oop/pratice.py
+++ Python/lecture/fun_coding/python_oop/pratice.py
@@ -1,19 +1,23 @@
-# quiz: 초간단 연습
-# type checker 데코레이터 만들기(인자 유효성 검사)
-# digit1, digit2를 곱한 값을 출력하는 함수 만들기
-# type_checker 데코레이터로 digit1, digit2가 정수가 아니면 only integer support 출력하고 끝냄
-# if(type(digit1) != int) or if(type(digit2) != int)
+# 파라미터에 관계없이 모든 함수에 적용 가능한 Decorator 만들기
+def generator_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("function is decorated")
+        return func(*args, **kwargs)
 
-def checker(func):
-    def inner_func(digit1, digit2):
-        if (type(digit1) != int) or (type(digit2) != int):
-            print("only integer support")
-        else:
-            func(digit1, digit2)
+    return wrapper
 
-    return inner_func
+@generator_decorator
+def calc_square(digit):
+    return digit ** 2
 
-@checker
-def multiply(digit1, digit2):
-    return digit1 * digit2
+@generator_decorator
+def calc_plus(digit):
+    return digit + digit
 
+@generator_decorator
+def calc_quad(digit1, digit2, digit3, digit4):
+    return digit1 * digit2 * digit3 * digit4
+
+calc_square(10)
+calc_plus(10)
+calc_quad(10, 20, 30, 40)
