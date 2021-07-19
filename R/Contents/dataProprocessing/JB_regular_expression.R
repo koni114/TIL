f3b1b0230a6e2b876c54becf87197cd2a3ab2131
jae hun hur <koni114@naver.com>
2021-07-18 21:52:39 -0700

    2021-07-18

--- R/Contents/dataProprocessing/JB_regular_expression.R
+++ R/Contents/dataProprocessing/JB_regular_expression.R
@@ -1,110 +1,114 @@
-#- regular expression
-
-#- 계좌 번호 금액만 추출하기
+#- reg expression
 text = "안녕하세요, 선생님의 계좌번호는 212-08-234562 입니다.
           계좌번호 1234-62-124521로 조회한 결과, 잔액은 200,300원 입니다.
           제 계좌번호는 616132-04-123456이고, 이름은 엘리스입니다. 제 대기번호를 알 수 있을까요?
           회원님의 현재 대기번호는 13번입니다. 감사합니다.
           직거래를 원하시면 계좌번호 1410-12-713088에 30,000원 선입금 바랍니다."
 
-p1 <- "\\d+-\\d+-\\d+"
-m1 <- str_extract_all(text, p1)
-print(m1)
+#- 1. 한자리 이상의 숫자 --> +
+exp <- "\\d+-\\d+-\\d+"
+stringr::str_extract_all(text, exp)[[1]]
 
-p2 <- "\\d+,\\d+"
-m2 <- str_extract_all(text, p2)
-print(m2)
+exp <- "\\d+,\\d+"
+stringr::str_extract_all(text, exp)[[1]]
 
-#- ^: 시작문자열
-#- ^is는 is 문자열로 시작하지 않기 때문에 아무것도 나오지 않음
-p1 <- "^Life"
-p2 <- "^is"
-m1 <- str_extract_all("Life is short", p1)
-m2 <- str_extract_all("Life is short", p2)
+#- 2. 시작문자열 : ^
+exp  <- "^Life"
+exp2 <- "^is"
 
-print(m1)
-print(m2)
+stringr::str_extract_all('Life is short', exp)[[1]]
+tt <- stringr::str_extract_all('Life is short', exp2)[[1]]
 
-#- $: 끝 문자열
-#- $는 제일 뒤에 붙여줘야 함
-p1 <- "short$"
-p2 <- "short$"
-m1 <- str_extract_all("Life is short",  p1)
-m2 <- str_extract_all("Life is short, art is long",  p2)
-print(m1)
-print(m2)
+#- 3. OR 연산자 사용. --> |
+exp <- "banana|apple"
+stringr::str_extract_all('I like banana and apple', exp)[[1]]
 
-#- OR 연산자 --> | 사용
-p3 <- "apple|banana" 
-m3 <- str_extract_all( "I like apple and banana", p3)
-print(m3)
+#- 4. []안의 ^ 연산자 --> not의 의미로 사용
+#-    aeiou가 아닌 문자를 추출하고 싶다면, "[^aeiou]" 사용
+exp <- "[^aeiou]"
+stringr::str_extract_all('Life is short', exp)[[1]]
 
-#- ^가 대괄호 안에 쓰이면 not의 의미를 가짐
-#- 따라서 aeiou가 아닌 char을 추출하고 싶으면 --> [^aeiou] 라고 입력
-p4 = "[^aeiou]"
-m4 = str_extract_all("Life is short, art is long", p4)
-print(m4)
+#- aeiou와 공백을 뺀 나머지 문자열 추출
+exp <- "[^aeiou\\s]"
+stringr::str_extract_all('Life is short', exp)[[1]]
 
-#- []와 - 이용하기
-#- [0-9]는 숫자, [a-z]는 알파벳 소문자, [A-Za-z]는 알파벳 대소문자와 매칭됨
-#- '-'를 사용할 때, 왼쪽에 오는 문자가 오른쪽에 오는 문자보다 유니코드 상 먼저오는 문자여야 함
+#- 5. 어떤 문자가 오더라도 매칭 -> .
+text <-  "cat bat c@t hat cut com cook cant"
+exp  <- "c.t"
+stringr::str_extract_all(text, exp)
 
-# 임의의 문자열
-text = "vkvJZZjgsr=B5Al83+#@04?+p%x7DI3k"
-p1 <- "[0-9]"     # 숫자와 매칭됨
-p2 <- "[a-z]"     # 알파벳 소문자와 매칭됨
-m1 <- str_extract_all(text, p1)
-m2 <- str_extract_all(text, p2)
-print(m1)
-print(m2)
-
-#- \d, \D 사용하기
-p1 <- '\d'
-p2 <- '\D'
-m1 = str_extract_all("el_ice%20$19", p1)
-m2 = str_extract_all("el_ice%20$19", p2)
-print(m1)
-print(m2)
-
-#- \w는 [A-Za-z0-9_] 의 의미를 가짐
-#- \W는 [^A-Za-z0-9_] 의 의미를 가짐
-p1 <- "\w"         #
-p2 <- "\W"         #
-m1 = str_extract_all("Life is short", p1)
-m2 = str_extract_all("Life is short", p2)
-print(m1)
-print(m2)
-
-#- \s는 공백 문자(white space)와 모두 매칭
-#- --> \t, \n, \v, \f, \r
-#- \S는 그 반대
-p1 <-"\s"   # 공백 문자와 매칭
-p2 <- "\S"  # 공백이 아닌 문자와 매칭
-m1 <- str_extract_all( "Life is short", p1)
-m2 <- str_extract_all("Life is short",  p2)
-print(m1)
-print(m2)
-
-#- . --> 어떤 문자가 오더라도 매칭되는 메타 문자
-#- 예를 들어 c.t 는 , “cat, cbt, cct, c@t, c$t, c t…”등의 문자열과 매칭
-text = "cat bat c@t hat cut com cook cant"
-p1 = "c.t"        # c로 시작하고 t으로 끝나는, 모든 3글자 단어에 매칭
-m1 = str_extract_all(text, p1)
-print(m1)
-
-#- (?i) --> 대소문자 구분 안한다는 메타 문자
-#- 해당 문자를 어디에 넣든 대소문자를 구분 안함
+#- 6. 대소문자 구분 안한다는 문자 -> (?i)
 text <- "APPLE APPLe APPlE APpLE ApPLE aPPLE APPle APpLe ApPLe aPPLe APplE ApPlE aPPlE AppLE aPpLE apPLE"
-p1 <- "APPLE"
-p2 <- "(?i)APPLE"       # 대소문자를 무시하며 APPLE에 매칭되는 패턴을 작성해보세요.
-m1 <- str_extract_all(p1, text)
-print("m1 결과 : ", m1)
-m2 = str_extract_all(p2, text)
-print("m2 결과 : ", m2)
+exp  <- "(?i)apple" 
+stringr::str_extract_all(text, exp)[[1]]
 
-#- 문자 개수를 지정 --> {n}
-#- ex) 숫자 4자리 --> \d{4}
+#- 7. 문자 개수를 지정 --> {n}
+#- ex) 숫자 4자리      --> {4}
 s = '111234a34a12'
-p1 = "\\d{4}\\D\\d{2}\\D\\d{2}$"    # 여기에 정규표현식을 입력하세요.
-m1 = str_extract_all(p1, s) 
-print(m1)
+exp = "\\d{4}\\D\\d{2}\\D\\d{2}$"
+stringr::str_extract_all(s, exp)[[1]]
+
+#- 8. 0개 이상 --> *
+text <- "ct cat caat caaat caaaaat caaaaaat cbt c1t c@t c_t"
+exp <- "ca*t"
+stringr::str_extract_all(text, exp)[[1]]
+
+#- 9. 1개 이상 --> +
+#- ex) like[a-z]+ : like는 매칭 안되고, likehood, likely...
+text <- "apple banana carrot rabbit"
+exp1 <- "[a-z]"  #- 영어 알파벳 소문자 매칭
+exp2 <- "[a-z]+" #- 영어 소문자 단어 매칭
+stringr::str_extract_all(text, exp1)[[1]]
+stringr::str_extract_all(text, exp2)[[1]]
+
+# 10. n개 매칭 --> {n}
+#     ** n개 이상          --> {n,} , 
+#        n개 이상 m개 이하 --> {n,m} ,
+#- ex) 7자리 이상인 수 : {7, }
+
+text <- "2 96 4019 884863 56635574 946482 95325201 410505 5802 6661337 2937786 31103"
+exp  <- "[0-9]{7,}"
+stringr::str_extract_all(text, exp)[[1]]
+
+
+# 11. ** 0또는 1개 --> ?
+#     사용할 때 뒤에 배치시켜야 함
+text <- "ac abc abbc abbbc abbbbbc"  
+exp  <- "ab?c"
+stringr::str_extract_all(text, exp)[[1]]
+
+# 12. ** 조심해야 할 Tip
+#- 수량자는 항상 가장 긴 부분과 매칭하려는 속성이 있음
+#- 만약 방지하려면 제일 마지막에 ?를 붙여야 함
+text <- "<html><head><Title>제목</head></html>"
+exp1 <- "<.*>"
+exp2 <- "<.*?>"
+
+stringr::str_extract_all(text, exp1)[[1]]
+stringr::str_extract_all(text, exp2)[[1]]
+
+#- 13. 그룹 사용하기
+#- at를 그룹핑해서 매칭 여부 확인
+text  <- "catatatatatat"
+exp   <- "c(at)+"
+stringr::str_extract_all(text, exp)[[1]]
+
+###############################
+## 정규 표현식 예제 풀어보기 ##
+###############################
+
+#- 1. s는 1개 또는 2개의 숫자로 시작한다.
+#-    그 다음, s에 4개 이상의 영어 소문자 알파벳이 온다.
+#-    맨 마지막에는, 온점 .이 0개이거나 1개 있다.
+#-    이외의 불필요한 문자가 있을 경우 모두 매치되지 않아야 한다.
+test <- "12abcdefg."
+exp = "^\\d{2}[a-z]{4,}[\\.]?$" 
+stringr::str_extract_all(test, exp)[[1]]
+
+#- 2. 전화번호를 찾아 010-****-1234 로 바꿔보기
+stringr::str_replace("010-9119-7025", "^(010)\\D?\\d{4}\\D?(\\d{4})", "010-****-\\g<2>")
+
+
+
+
+
--- R/Contents/libraryUsage/stringr.R
+++ R/Contents/libraryUsage/stringr.R
@@ -1,13 +1,64 @@
-#- stringr.
+#' stringr library를 통한 문자열 처리
+#' stringr package가 제공되는 함수
+#' - str_length(): 문자열 길이 반환 
+#' - str_c(): 문자열 연결, str_join()의 개선형
+#' - str_sub(): 범위에 해당하는 부분 문자열 생성
+#' - str_split(): 구분자를 기준으로 문자열을 분리하여 부분 문자열 생성
+#' - str_replace(): 기존 문자열을 특정 문자열로 교쳬
+#' - str_extract(): 문자열에서 특정 문자열 패턴의 첫번째 문자열 추출
+#' - str_extract_all(): 문자열에서 특정 문자열 패턴의 모든 문자열 추출
+#' - str_locate(): 문자열에서 특정 문자열 패턴의 첫 번째 위치
+#' - str_locate_all(): 문자열에서 특정 문자열 패턴의 모든 위치 찾기
 library(stringr)
+myStr <- "Hongkd1051Leess1002YOU25홍길동2005"
+stringr::str_length(myStr)
+stringr::str_locate(myStr, '홍길동')
+stringr::str_sub(myStr, locate[1], locate[2])
+stringr::str_to_upper(myStr)
+stringr::str_to_lower(myStr)
 
-#- str_c: 문자열 결합
-str_c('a', 'b','c', sep = '+')
+myStr <- "Hongkd1051,Leess1002,YOU25,홍길동2005"
+stringr::str_replace_all(myStr, 'Hong', 'KIM')
+stringr::str_c(myStr,",이순신2019")
 
-#- str_length : 문자의 개수
-str_length(c('abcd', 'abc'))
+str1 <- c("홍길동","김길동","이순신","강감찬")
+paste(str1,collapse=",")
 
-#- ** str_extract: 부분 문자열 추출
-hw <- "Hadley Wickham"
+myStr <- "Hongkd1051,Leess1002,YOU25,홍길동2005"
 
-str_extract(hw, 'Hadley')
+# 영문소문자 연속 3문자 추출
+stringr::str_extract_all(myStr, "[a-z]{3}")
+
+# 영문소문자 연속 3문자 이상 추출
+stringr::str_extract_all(myStr, "[a-z]{3,}")
+
+# 영문소문자 연속 3~5문자 추출
+stringr::str_extract_all(myStr, "[a-z]{3,5}")
+
+# 해당 문자열 추출
+stringr::str_extract_all(myStr, 'Hong')
+
+# 연속된 3개의 한글 문자 추출
+stringr::str_extract_all(myStr, "[가-힣]{3}")
+
+# 연속된 4개의 숫자문자 추출
+stringr::str_extract_all(myStr, "[0-9]{4}")
+
+# 영문 소문자 제외한 나머지 추출
+stringr::str_extract_all(myStr, "[^a-z]")
+
+# 한글을 제외한 나머지 연속된 5개 추출
+stringr::str_extract_all(myStr, "[^가-힣]{5}")
+
+# 숫자를 제외한 나머지 연속된 3개 추출
+stringr::str_extract_all(myStr, "[^0-9]{3}")
+
+# w는 한글,영문자,숫자문자를 포함하지만 특수문자는 제외
+stringr::str_extract_all(myStr, "\\w{6}")
+
+#- 하나의 컬럼에 있는 데이터를 쪼개 두 개의 컬럼으로 생성
+test <- c('hello/world', 'my/name', 'is/HJH')
+do.call('rbind', strsplit(test, '/')) %>% data.frame %>% rename(first_name = X1, last_name = X2)
+
+
+
