ab79fd9d69374900c41c5c3b921eb3397d3c27ac
jae hun hur <koni114@naver.com>
2021-10-04 23:35:51 -0700

    2021-10-04

--- Machine-Learning/lecture/hands_on_ML/practice.py
+++ Machine-Learning/lecture/hands_on_ML/practice.py
@@ -1,14 +1,108 @@
-from sklearn import datasets
-import numpy as np
-iris = datasets.load_iris()
-X = iris["data"][:, (2, 3)]
-y = iris["target"]
+# 역순 연결 리스트
+import sys
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-#- 모든 샘플에 편향 추가 xo = 1
-X_with_bias = np.c_[np.ones([len(X), 1]), X]
 
-test_ratio = 0.2
-validation_ratio = 0.2
-total_size = len(X_with_bias)
+def reverse_linklist(head: ListNode):
+    node, prev = head, None
 
-test_size = int(total_size * test_ratio)
+    while node:
+        next, node.next = node.next, prev
+        prev, node = node, next
+
+    return prev
+
+# 리스트를 연결 리스트로 변경하는 코드
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def list_to_linkedlist(lists, l1:ListNode):
+    prev = None
+    for value in lists:
+        node = ListNode(value)
+        prev.next = node
+        prev = node
+
+    lists = []
+    while l1:
+        lists.append(l1.val)
+        l1 = l1.next
+
+
+# 연결 리스트를 이용해서 stack 을 구현.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.last = ListNode()
+
+    def push(self, value):
+        self.last = ListNode(value, self.last)
+
+    def pop(self):
+        item = self.last.val
+        self.last = self.last.next
+        return item
+
+
+# tmp 를 사용하지 않고, a, b를 swap 할 수 있는 방법 ?
+a, b = 10, 20
+a = a + b  # a = a + b
+b = a - b  # b = a
+a = a - b
+print(a, b)
+
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.q = deque()
+
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        toggle = 0
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                toggle = 1
+        if toggle == 0:
+            break
+    return arr
+
+bubble_sort([1, 2, 10, 4, 5])
+
+# 선택 정렬.
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_num, idx = arr[i], i
+        for j in range(i, len(arr)):
+            if arr[j] < min_num:
+                min_num, idx = arr[j], j
+        arr[i], arr[idx] = min_num, arr[i]
+    return arr
+
+selection_sort([1, 4, 3, 2, 10])
+
+# 삽입 정렬.
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        curr_num = arr[i]
+        left = i - 1
+        while left >= 0 and arr[left] > curr_num:
+            arr[left + 1] = arr[left]
+            arr[left] = curr_num
+            left -= 1
--- Python/daily_Python/module.py
+++ Python/daily_Python/module.py
@@ -1,5 +1,38 @@
-l = 9
-r = 11
+# 특정 아이디 문자열 A와 B가 있을 때, 다음 코드를 작성 하십시오.
+# 1. '유사도'의 기준을 정의하고, 두 문자열 간의 유사도룰 구하는 코드를 구현 하시오.
+# 2.  비교 아이디 문자열을 원본 아이디 문자열과 가장 유사한 순서대로 정렬 하시오.
+#
+# 유사도 알고리즘 예시: Jaccard Distance
+# - J(A, B) = count(교집합) / count(합집합)
+#
+# 예시)
+# 원본 아이디 문자열: "abcd1234"
+# 비교 아이디 문자열 리스트: ["abcd", "ab1234", "abxz0011", "1234abcd", "ABCD1234", "abcd1234abcd1234"]
 
-def oddNumbers(l, r):
-    return [value for value in range(l, r+1) if value % 2 == 1]
+
+def jaccard_dist(check_s, comp_list):
+    simul_list = []
+
+    #- 교집합 / 합집합
+    for s in comp_list:
+        simul_list.append((s, mk_jac(check_s, s)))
+
+    simul_list = sorted(simul_list, key=lambda x: -x[1])
+    return simul_list
+
+
+def mk_jac(s1, s2):
+    s1_set, s2_set = set(s1), set(s2)
+    return len(s1_set.intersection(s2_set)) / len(s1_set.union(s2_set))
+
+
+if __name__ == '__main__':
+    check_s = "abcd1234"
+    comp_list = ["abcd", "ab1234", "abxz0011", "1234abcd", "ABCD1234", "abcd1234abcd1234"]
+    jaccard_dist(check_s, comp_list)
+
+
+
+
+
+
