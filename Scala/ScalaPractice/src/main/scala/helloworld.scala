fac91c37124a9a658f5f1845cfab3e00e7739aae
jae hun hur <koni114@naver.com>
2021-11-03 08:17:50 -0700

    2021-11-03

--- Scala/ScalaPractice/src/main/scala/helloworld.scala
+++ Scala/ScalaPractice/src/main/scala/helloworld.scala
@@ -1,49 +1,22 @@
 object HelloWorld {
   def main(args: Array[String]): Unit ={
 
-    val x: Int = 5
-    println(x * 5) // -- 2
-    println(x / 5) // -- 3
+    def combination(x:Int, y:Int, f: (Int, Int) => Int) = f(x , y)
+    println(combination(10, 20, _ * _))
 
-    var y: Double = 0.25
-    y = 355.0 / 113.0
+    def tripleOp[A, B](a: A, b: A, c: A, f: (A, A, A) => B) = f(a, b, c)
+    println(tripleOp[Int, Boolean](93, 92, 14, _ > _ + _))
+
+    def factorOf(x: Int, y: Int) = y % x == 0
+    val f = factorOf _
+    println(f(10, 20))
+
+    val multiple3 = factorOf(3, _ : Int)
+    val y  = multiple3(78)
     println(y)
 
-    // val x = 20
-    // val greeting = "hello, world"
-    val atSymbol = '@'
 
-    val l: Long = 20
-    val i: Int = l.toInt
-    println(i)
-
-    val hello = "Hello, there"
-    val greeting = "Hello," + "World"
-    val matching = (greeting == "Hello, there")
-    val theme = "Na " * 16 + "Batman"
-
-    println(hello + " " + greeting)
-
-    // 중괄호를 이용한 문자열 출력 예제
-    val item = "apple"
-    println(s"How do you like them ${item}s?")
-
-    val input = "Enjoying this apple 3.14159 times today"
-    val pattern = """.* apple ([\d.]+) times .*""".r
-    val pattern(amountText) = input
-    val amount = amountText.toDouble
-    println(amount)
-
-    val isFalse = !true
-    val isTrue = true
-    val unequal = (5 != 6)
-    val isLess = (5 < 6)
-    val unequalAndLess = unequal & isLess
-    val definitelyFalse = false && unequal
-
-    //
-
-    
 
   }
+
 }
