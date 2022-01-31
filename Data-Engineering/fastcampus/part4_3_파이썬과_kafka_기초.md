532b4abef2bbd2942b7ef5d3151c74c375d7b5fd
jae hun hur <koni114@naver.com>
2022-01-30 20:04:44 -0800

    2021-01-30

--- Data-Engineering/fastcampus/part4_3_파이썬과_kafka_기초.md
+++ Data-Engineering/fastcampus/part4_3_파이썬과_kafka_기초.md
@@ -54,7 +54,6 @@ services:
     environment:
       ZOO_MY_ID: 1
       ZOO_PORT: 2181
-      ZOO_SERVERS: server.1=zookeeper:2888:3888
     volumes:
       - ./data/zookeeper/data:/data
       - ./data/zookeeper/datalog:/datalog
@@ -66,3 +65,140 @@ services:
   버전은 7.0.0 사용
 - confluent 는 kafka를 만든 팀이 나와서 설립한 별도 회사 
 - `ports` 는 host 와 docker 를 연결해주기 위한 port
+- 위의 Zookeeper instance 생성을 위한 docker-compose file 에 추가하면 됨
+~~~yml
+  kafka1:
+    image: confluentinc/cp-kafka:7.0.0
+    hostname: kafka1
+    ports:
+      - "9091:9091"
+    environment:
+      KAFKA_ADVERTISED_LISTENERS: LISTENER_DOCKER_INTERNAL://kafka1:19091,LISTENER_DOCKER_EXTERNAL://${DOCKER_HOST_IP:-127.0.0.1}:9091
+      KAFKA_LISTENER_SECURITY_PROTOCOL_MAP: LISTENER_DOCKER_INTERNAL:PLAINTEXT,LISTENER_DOCKER_EXTERNAL:PLAINTEXT
+      KAFKA_INTER_BROKER_LISTENER_NAME: LISTENER_DOCKER_INTERNAL
+      KAFKA_ZOOKEEPER_CONNECT: "zookeeper:2181"
+      KAFKA_BROKER_ID: 1
+      KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR: 1
+    volumes:
+      - ./data/kafka1/data:/tmp/kafka-logs
+    depends_on:
+      - zookeeper
+  kafka2:
+    image: confluentinc/cp-kafka:7.0.0
+    hostname: kafka1
+    ports:
+      - "9092:9092"
+    environment:
+      KAFKA_ADVERTISED_LISTENERS: LISTENER_DOCKER_INTERNAL://kafka1:19092,LISTENER_DOCKER_EXTERNAL://${DOCKER_HOST_IP:-127.0.0.1}:9092
+      KAFKA_LISTENER_SECURITY_PROTOCOL_MAP: LISTENER_DOCKER_INTERNAL:PLAINTEXT,LISTENER_DOCKER_EXTERNAL:PLAINTEXT
+      KAFKA_INTER_BROKER_LISTENER_NAME: LISTENER_DOCKER_INTERNAL
+      KAFKA_ZOOKEEPER_CONNECT: "zookeeper:2181"
+      KAFKA_BROKER_ID: 2
+      KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR: 1
+    volumes:
+      - ./data/kafka1/data:/tmp/kafka-logs
+    depends_on:
+      - zookeeper
+  kafka3:
+    image: confluentinc/cp-kafka:7.0.0
+    hostname: kafka1
+    ports:
+      - "9093:9093"
+    environment:
+      KAFKA_ADVERTISED_LISTENERS: LISTENER_DOCKER_INTERNAL://kafka1:19093,LISTENER_DOCKER_EXTERNAL://${DOCKER_HOST_IP:-127.0.0.1}:9093
+      KAFKA_LISTENER_SECURITY_PROTOCOL_MAP: LISTENER_DOCKER_INTERNAL:PLAINTEXT,LISTENER_DOCKER_EXTERNAL:PLAINTEXT
+      KAFKA_INTER_BROKER_LISTENER_NAME: LISTENER_DOCKER_INTERNAL
+      KAFKA_ZOOKEEPER_CONNECT: "zookeeper:2181"
+      KAFKA_BROKER_ID: 2
+      KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR: 1
+    volumes:
+      - ./data/kafka1/data:/tmp/kafka-logs
+    depends_on:
+      - zookeeper
+~~~
+- `ports` : local com 과 docker 를 연결주기 위한 port 설정  
+   3개의 kafka(broker)를 사용하기 위해 port를 9091, 9092, 9093 으로 설정
+- `environment`
+  - `KAFKA_ADVERTISED_LISTENERS` kafka listener 설정  
+  docker_internal, docker_external 설정
+  - `KAFKA_LISTENER_SECURITY_PROTOCOL_MAP`: 
+  - `KAFKA_ZOOKEEPER_CONNECT` : zookeeper 와 연결하기 위한 connection 정보 삽입
+- `volumes` : kafka 와 local host 상의 directory mount 설정
+- `depends_on` : zookeeper 뒤에 kafka 를 실행시키기 위해 설정
+- 주의해야 할 사항
+  - kafka broker id 는 동일한 id가 2개가 있으면 안됨
+  - docker-compose 를 여러번 수행할 시 에러가 발생할 수 있는데, 이 때 `docker-compose rm -svf` 명령어를 실행하고 다시 수행해야 함
+- kafka instance를 3개 만드는 것이므로, 위의 kafka1 instance option을 복사해서, `kafka2`, `kafka3`으로 만들고, 해당 port 를 각각 9092, 9093으로 변경하고, broker_id도 2, 3으로 변경
+
+### kafka 클러스터 구축하기 - Kafdrop 
+- kafka broker 를 조금 더 쉽게 관리해주는 tool --> kafdrop
+- kafka를 web UI를 통해 눈으로 확인하게 해주는 tool
+- kafdrop은 docker-hub에서 kafdrop으로 존재함 
+- docker-compose file 에 추가해보자
+~~~yml
+kafdrop:
+    image: obsidiandynamics/kafdrop
+    restart: "no"
+    ports:
+      - "9000:9000"
+    environment:
+      KAFKA_BROKER_CONNECT: "kafka1:19091"
+    depends_on:
+      - kafka1
+      - kafka2
+      - kafka3
+~~~
+- `ports`: 9000:9000 으로 설정하게 되면, instance 실행시 localhost:9000 으로 실행 가능
+- `depends_on`: kafka image 3개가 모두 설치된 이후에 설치되어야 함
+
+### kafka 클러스터 구축하기 - Topic 만들기
+- 클러스터 안에서 쓸 topic 만들어보기
+- `docker-compose up -d` 로 입력시, 모든 image 들이 background 로 돌게 됨
+- kafka topic 을 만들기 위해서는 kafka instance 안에서 만들어야 함
+- 그러기 위해서는 `docker exec -it 03-kafka_kafka1_1` 명령어 사용  
+  이는 docker 내 kafka의 이름(docker app에서 확인 가능)
+~~~shell
+docker exec -it 03-kafka_kafka1_1 kafka-topics --bootstrap-server=localhost:19091 --create --topic first-cluster-topic --partitions 3 --replication-factor 1
+~~~
+- 위의 명령어 수행시 first-cluster-topic 이라는 topic 이 추가되며, partition도 3개가 추가되는 것을 확인 가능
+- kafdrop 을 사용시, 굉장히 쉽게 관리가 가능
+
+### Kafka 클러스터를 이용하는 Producer
+- `cluster_producer.py` 생성하여, 3개의 kafka producer 를 사용할 수 있게 해보자
+~~~Python
+# cluster_producer.py
+from kafka import KafkaProducer
+
+brokers = ["localhost:9091", "localhost:9092", "localhost:9093"]
+topic_name = "first-cluster-topic"
+
+producer = KafkaProducer(bootstrap_servers=brokers)
+producer.send(topic_name, b"Hello cluster world")
+producer.flush()
+~~~
+- 위의 cluster_producer.py 를 실행시키면, 메세지가 producer 로 전송됨
+
+### Kafka 클러스터를 이용하는 Consumer
+- `cluster_consumer.py`를 생성하여, consumer 를 만들어보자
+~~~python
+from kafka import KafkaConsumer
+brokers = ["localhost:9091", "localhost:9092", "localhost:9093"]
+consumer = KafkaConsumer("first-cluster-topic", bootstrap_servers=brokers)
+
+# 무한 루프를 돌면서 메세지를 확인하는 구문
+for message in consumer:
+    print(message)
+~~~
+- 위의 script 실행 전, docker instance 가 정상적으로 실행되고 있는지 확인
+
+#### producer-consumer test
+- 먼저 하나의 terminal 창에서 `python cluster_consumer.py` 실행
+- 다른 하나의 terminal 창에서 `python cluster_producer.py`를 실행하면, 해당 스크립트에서 작성한 message가 정상적으로 송신되는 것을 확인
+
+### kafdrop으로 메세지 확인하기
+- kafdrop UI 에서 topic 클릭 후, View-Messages 를 클릭하면 각각의 message 들을 확인 가능
+
+
+### 용어 정리
+- listener
+  - 특정 이벤트(특정한 사건)이 발생하기를 귀 귀울여 기다리다가 실행되는 컴포넌트(메소드, 함수) 
--- Python/daily_Python/2021-01-26.py
+++ Python/daily_Python/2021-01-26.py
@@ -1,49 +1,174 @@
-# 2021.01.26.py
-########################################
-# test, csv file 불러오기 : pd.read_csv() #
-########################################
+import pandas as pd
+df_left = pd.DataFrame({'KEY': ['K0', 'K1', 'K2', 'K3'],
+                        'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': [0.5, 2.2, 3.6, 0.4]})
+
+df_right = pd.DataFrame({'KEY': ['K2', 'K3', 'K4', 'K5'],
+                         'C': ['C2', 'C3', 'C4', 'C5'],
+                         'D': ['D2', 'D3', 'D4', 'D5']})
+
+df_all = pd.merge(df_left, df_right,  how='outer', on='KEY')
+
+
+df_all.isnull()
+df_all.notnull()
+
+df_all.loc[[0, 1], ['A', 'B']] = None
+df_all[['A', 'B']].isnull()
+
+# 컬럼별 결측값 개수 구하기: df.isnull().sum()
+df_all.isnull().sum()
+df_all['A'].isnull().sum()
+
+# 컬럼별 결측값이 아닌 값의 개수를 구하려면, df.notnull().sum() 을 사용
+df_all.notnull().sum()
+df_all['NaN_cnt'] = df_all.isnull().sum(1)
+df_all['NotNull_cnt'] = df_all.notnull().sum(1)
+print(df_all)
+
+
+
+## 결측값 채우기, 대체, 결측값 처리 : df.fillna()
+import numpy as np
+import pandas as pd
+df = pd.DataFrame(np.random.randn(5, 3), columns=['C1', 'C2', 'C3'])
+
+df.loc[0, 0] = None
+df.loc[1, ['C1', 'C3']] = np.nan
+df.loc[2, ['C2']] = np.nan
+df.loc[3, ['C2']] = np.nan
+df.loc[4, 'C3'] = np.nan
+
+# 결측값을 특정 값으로 채우기 : df.fillna(0)
+df_0 = df.fillna(0)
+df_missing = df.fillna('missing')
+
+# 결측값 앞 방향으로 채워나가는 경우, method = 'ffill', 'pad' 입력
+df_ffill = df.fillna(method='ffill')
+df_pad = df.fillna(method='pad')
+
+# 결측값 뒷 방향으로 채워나가는 경우 method = 'bfill', method = 'backfill'
+df_bfill = df.fillna(method='bfill')
+df_backfill = df.fillna(method='backfill')
+print(df_backfill)
+
+# 앞/뒤 방향으로 결측값 채우는 횟수를 제한하기 : fillna(limit=number)
+df.fillna(method='ffill', limit=1)
+df.fillna(method='bfill', limit=1)
+
+# 결측값을 변수별 평균으로 대체하기 : df.fillna(df.mean()),
+#                            df.where(pd.notnull(df), df.mean(), axis='columns')
+# df.where(Series 객체에 대한 조건문, 거짓 값에 대한 대체 값)
+
+df.fillna(df.mean())
+df.where(pd.notnull(df), df.mean(), axis='columns')
+
+# 다음은 C1 컬럼의 평균을 가지고, C1, C2, C3 컬럼의 결측값 대체
+df.mean()['C1']
+df.fillna(df.mean()['C1'])
+
+# 'C1' , 'C2' 컬럼에 대해서만 각 컬럼의 평균을 가지고, 각 컬럼에 있는 대체값을 대체하는 경우
+df.mean()['C1':'C2']
+df.fillna(df.mean()['C1':'C2'])
+
+# 결측값을 다른 변수의 값으로 대체
+# C2 컬럼에서 결측값이 없으면 'C2' 컬럼의 값을 그대로 사용,
+# C2 컬럼에 결측값이 있으면 C1 컬럼의 값을 가져다가 결측값 채우기
+
+df_2 = pd.DataFrame({'C1': [1, 2, 3, 4, 5],
+                     'C2': [6, 7, 8, 9, 10]})
+
+df_2.loc[[1, 3], ['C2']] = np.nan
+
+df_2['C2_New'] = np.where(pd.notnull(df_2['C2']) == True, df_2['C2'], df_2['C1'])
+
+# loop programming 으로 계산하는 경우, 동일한 결과를 반환하지만,
+# np.where() 와 pd.notnull() 을 사용하는 것이 속도도 빠르고 코드도 짧음
+
+# 결측값 있는 행 제거 : dropna(axis=0)
+# 결측값 있는 열 제거 : dropna(axis=1)
 
 import pandas as pd
-import os
-os.getcwd()
+import numpy as np
+df = pd.DataFrame(np.random.randn(5, 4), columns=['C1', 'C2', 'C3', 'C4'])
+df.loc[[0, 1], 'C1'] = None
+df.loc[2, 'C2'] = np.nan
 
-df = pd.read_csv('./test_data/test_csv_file.csv')
-print(df.shape)
+# 결측값이 들어있는 행 전체 삭제
+df.dropna(axis=0)
 
-df = pd.read_csv('./test_data/test_text_file.txt', sep="|")
-print(df)
-print(df.shape)
+# 결측값이 들어있는 열 전체 삭제
+df.dropna(axis=1)
 
-# 구분자가 tab 이라면, sep  = '\t' 입력
+# 특정 행 또는 열을 대상으로 결측값이 들어있으면 제거
+df['C1'].dropna()
+df[['C1', 'C2', 'C3']].dropna()
+df[['C1', 'C2', 'C3']].dropna(axis=1)
 
-# 파일 불러올 때, index
-df = pd.read_csv('./test_data/test_text_file.txt', sep="|", index_col='ID')
-df = pd.read_csv('./test_data/test_text_file.txt', sep="|", index_col=0)
-print(df)
+# 결측값 보간(interpolation)하기.
+# (1) 시계열 데이터의 값에 선형으로 비례하는 방식으로 결측값 보간
+# (2) 시계열 날짜 index 기준으로 결측값 보간
+# (3) DataFrame 값에 선형으로 비례하는 방식으로 결측값 보간
+# (4) 결측값 보간 개수 제한
 
-# 변수 이름(column name) 이 없는 파일 불러올때 이름 부여하기
-df = pd.read_csv('./test_data/text_without_column_name.txt', sep="|", names=['ID', 'A', 'B', 'C', 'D'],
-                 header=None, index_col='ID')
-print(df)
+import pandas as pd
+import numpy as np
+from datetime import datetime
 
-# unicode decode error 발생시, encoding parameter 지정
-file = pd.read_csv('directory/file', sep='|', encoding='CP949')
-file = pd.read_csv('directory/file', sep='|', encoding='latin')
+date_strs = ['2021-12-01', '2021-12-03', '2021-12-04', '2021-12-10']
+dates = pd.to_datetime(date_strs)
 
-# 특정 줄은 제외하고 불러오기
-csv_2 = pd.read_csv('./test_data/test_text_file.txt', skiprows=[1, 2])
+ts = pd.Series([1, np.nan, np.nan, 10], index=dates)
 
-# n 개의 행만 불러오기
-csv_3 = pd.read_csv('./test_data/test_text_file.txt', nrows=3)
+ts_intp_linear = ts.interpolate()
+print(ts_intp_linear)
 
-# 사용자 정의 결측값 기호(custom missing value symbols) : na_values
-df = pd.read_csv('./test_data/test_text_file.txt', na_values=['?', '??', 'N/A', 'NA', 'nan'])
+# 시계열 날짜 index 기준으로 결측값 보간
+ts_intp_time = ts.interpolate(method='time')
+print(ts_intp_time)
 
-# 데이터 유형 설정 : dtype
-# pandas 는 데이터셋을 읽어들일 때 '첫번째 행의 데이터' 를 기준으로 각 컬럼별 데이터 유형을 추정해서 자동으로 셋팅함
-# 맞지 않는 경우, dtype 옵션으로 사전형으로 각 칼럼 별 데이터 유형을 짝 지어 명시적으로 설정 가능
-df = pd.read_csv('./test_data/test_text_file.txt', dtype={'ID': int,
-                                                          'LAST_NAME': str,
-                                                          'AGE': float})
+# DataFrame 값에 선형으로 비례하는 방식으로 결측값 보간
+df = pd.DataFrame({'C1': [1, 2, np.nan, np.nan, 5],
+                   'C2': [6, 8, 10, np.nan, 20]})
+
+df_intp_values = df.interpolate(method='values')
+print(df_intp_values)
+
+# 결측값 보간 개수 제한하기 : limit = 1
+df.interpolate(method='values', limit=1)
+
+# limit_direction = 'backward'
+df.interpolate(method='values', limit=1, limit_direction='backward')
+
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.arange(10).reshape(5, 2),
+                  index=['a', 'b', 'c', 'd', 'e'],
+                  columns=['C1', 'C2'])
+
+df.loc[['b', 'e'], ['C1']] = None
+df.loc[['b', 'c'], ['C2']] = None
 
 
+df = pd.DataFrame(np.random.randn(5, 3), columns=['C1', 'C2', 'C3'])
+df.iloc[0, 0] = None
+df.loc[1, ['C1', 'C3']] = np.nan
+df.loc[2, 'C2'] = np.nan
+df.loc[3, 'C2'] = np.nan
+df.loc[4, 'C3'] = np.nan
+
+df = pd.DataFrame(np.random.randn(5, 4),
+                  columns=['C1', 'C2', 'C3', 'C4'])
+
+df.loc[[0, 1], 'C1'] = None
+df.loc[2, 'C2'] = np.nan
+
+date_strs = ['2021-01-03', '2021-01-04', '2021-01-07', '2021-01-10']
+dates = pd.to_datetime(date_strs)
+ts = pd.Series([1, np.nan, np.nan, 10], index=dates)
+print(ts)
+
+df = pd.DataFrame({'C1': [1, 2, np.nan, np.nan, 5],
+                   'C2': [6, 8, 10, np.nan, 20]})
+
+df_intp_values = df.interpolate(method='values')
