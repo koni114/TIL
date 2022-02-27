29a98608da276c98dc38bfd0ddce2c2ee4fd3ca8
jae hun hur <koni114@naver.com>
2022-02-27 14:38:01 +0900

    check

---
+++ Python/daily_Python/git_push.sh
@@ -0,0 +1,18 @@
+Y=$(date + %Y)
+M=$(date + %m)
+D=$(date + %d)
+
+Ym=$Y-$M
+Ymd=%Y-%M-%d
+GitRep= "TIL"
+
+HomeDir="/Users/heojaehun/gitRepo"
+GitDir="$HomeDir/$GitRep"
+
+echo "push git from $GitDir --> $GitRep"
+
+cd $GitDir
+git init
+git add .
+git commit -m "commit $Ymd"
+git push origin master
