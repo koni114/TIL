fc0522d4522b1f16803f73f46d978e958d2c5f26
jae hun hur <koni114@naver.com>
2022-03-07 23:50:00 +0900

    commit 2022-03-

--- Python/daily_Python/practice.py
+++ Python/daily_Python/practice.py
@@ -1,3 +1,21 @@
+import os
+from threading import Thread
+import random
+
+class InputData:
+    def read(self):
+        raise NotImplementedError
+
+
+class PathInputData(InputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+
 class Worker:
     def __init__(self, input_data):
         self.input_data = input_data
@@ -6,9 +24,58 @@ class Worker:
     def map(self):
         raise NotImplementedError
 
-    def reduce(self):
+    def reduce(self, other):
         raise NotImplementedError
 
-class LineCounterWorker(Worker):
+
+class LineCountWorker(Worker):
     def map(self):
         data = self.input_data.read()
+        self.result = data.count("\n")
+
+    def reduce(self, other):
+        self.result += other.result
+
+
+def generate_inputs(data_dir):
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+
+def create_workers(input_list):
+    workers = []
+    for input_data in input_list:
+        workers.append(LineCountWorker(input_data))
+    return workers
+
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+
+    first, * rest = workers
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
+
+def mapreduce(data_dir):
+    inputs = generate_inputs(data_dir)
+    workers = create_workers(inputs)
+    return execute(workers)
+
+def write_test_files(tmpdir):
+    os.makedirs(tmpdir)
+    for i in range(100):
+        with open(os.path.join(tmpdir, str(i)), 'w') as f:
+            f.write('\n' * random.randint(0, 100))
+
+
+tmpdir = 'test_inputs'
+write_test_files(tmpdir)
+
+result = mapreduce(tmpdir)
+
+
+
+
+
+
