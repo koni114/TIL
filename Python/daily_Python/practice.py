5d97e945e06bfca78e8b46ef0cd0bb95a9f0d579
jae hun hur <koni114@naver.com>
2022-03-18 23:50:01 +0900

    commit 2022-03-

--- Python/daily_Python/practice.py
+++ Python/daily_Python/practice.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import numpy as np
 
-# 다음의 데이터를 활용하여 연속형 변수 C1 을 이산형화 시켜보세요
-# - C1 컬럼의 데이터를 10개의 구간으로 나누고, 이를 (1 ~ 10) 까지로 만들어본 후, C1_bin 컬럼에 넣어 보세요.
-# - 만든 C1_bin 컬럼 값을 다시 one-hot encoding 을 수행해보세요
-# - 이 때, 첫 번째 변수를 삭제해보세요
+import pandas as pd
+personnel_df = pd.DataFrame({'sequence': [1, 3, np.nan],
+                             'name': ['park', 'lee', 'choi'],
+                             'age': [30, 20, 40]})
 
-from sklearn.preprocessing import MinMaxScaler
+personnel_df.sort_values(["sequence"], ascending=False)
+personnel_df.sort_values(["sequence"], na_position="first")
+personnel_df.sort_values(["sequence"], na_position="last")
 
-np.random.seed(10)
-df = pd.DataFrame({'C1': np.random.randn(20),
-                   'C2': ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
-                          'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b']})
+personnel_tuple = [(1, 'park', 30),
+                   (3, 'lee', 20),
+                   (2, 'choi', 40)]
 
-np_lin = np.linspace(np.min(df["C1"]), np.max(df["C1"]), 10)
-df["C1_bin"] = np.digitize(df["C1"], np_lin)
-
-pd.get_dummies(df["C1_bin"], prefix="x", drop_first=True)
+s = pd.Series([0, 1, 2, 3, 4], index=['a', 'b', 'c', 'd', 'e'])
 
