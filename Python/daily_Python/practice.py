6e44269390b4a74a2fc51a86c2b31d9d31242512
jae hun hur <koni114@naver.com>
2022-04-13 23:44:30 +0900

    2021-04-13

--- Python/daily_Python/practice.py
+++ Python/daily_Python/practice.py
@@ -16,3 +16,22 @@ personnel_tuple = [(1, 'park', 30),
 
 s = pd.Series([0, 1, 2, 3, 4], index=['a', 'b', 'c', 'd', 'e'])
 
+# 다음 소스 코드를 완성하여 문자열에서 특정 단어가 있으면 True,
+# 없으면 False 가 출력되게 만드세요. find 함수는 코루틴으로 작성해야 합니다.
+
+def find(word):
+    toggle = True
+    while True:
+        x = (yield toggle)
+        toggle = word in x
+        print(f"toggle --> {toggle}")
+
+
+f = find('Python')
+next(f)
+
+print(f.send('Hello, Python!'))
+print(f.send('Hello, world!'))
+print(f.send('Python Script'))
+
+f.close()
